d7345be46c2dab85b662478a7c46686900127674
MR᭄﹅ MAHABUB﹅ メꪜ <mrmahabub345@gmail.com>
2025-01-20 18:11:25 +0600

    Update love.js

--- js/love.js
+++ js/love.js
@@ -176,7 +176,7 @@
 
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
-            ctx.font = "12px 微软雅黑,Verdana";
+            ctx.font = "12px 马哈布·拉赫曼,Verdana";
             ctx.fillText("To My Better Half", 23, 10);
             ctx.restore();
         },
